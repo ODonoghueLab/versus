74c74b19ef7a6ad09fe495ee31dcfa215e8766fd
Veemon <xxtheforgottenliesxx@gmail.com>
2016-09-03 20:39:24 +1000

    min password length, synchrous duplicate email checking

--- app/routes/register.js
+++ app/routes/register.js
@@ -23,6 +23,7 @@ module.exports = (app) => {
     req.checkBody('email', 'Please Enter Your Email').notEmpty();
     req.checkBody('password', 'Please Enter Both Password Fields').notEmpty();
     req.checkBody('passwordv', 'Please Enter Both Password Fields').notEmpty();
+    req.checkBody('password', 'Please Enter A Longer Password').len(8);
     req.checkBody('password', 'Passwords Do Not Match').equals(req.body.passwordv);
 
     var errors = req.validationErrors();
@@ -38,36 +39,51 @@ module.exports = (app) => {
 
     } else {
 
-      //Create New User Model
-      var newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      });
+      //Query if email already in use
+      var queryTest = User.findOne({email:req.body.email});
 
-      //Save to database
-      User.createUser(newUser, (err, user) => {
+      queryTest.then((doc) => {
 
-        //In Case of Error
-        if(err){
+        if(doc){
 
-          //Render Error Page
-          res.render('error', {
-            message: "Internal Error, Could Not Create User",
-            error: err
+          //Duplicate Email
+          res.render('register',{
+            errors: ["Email already in use"]
           });
-
-          console.log("Create User error");
-          throw err;
 
         } else {
 
-          //Render the home page with user's name
-          res.render('dash', {
-            name: user.name,
-            title: "Versus"
+          //Create New User Model
+          var newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
           });
 
+          //Save to database
+          User.createUser(newUser, (err, user) => {
+
+            //In Case of Error
+            if(err){
+
+              //Render Error Page
+              res.render('error', {
+                message: "Internal Error, Could Not Create User",
+                error: err
+              });
+
+              console.log("Create User error");
+              throw err;
+
+            } else {
+
+              //Render the home page with user's name
+              res.render('dash', {
+                name: user.name,
+                title: "Versus"
+              });
+            }
+          });
         }
       });
     }
