31bf9e907b1e57fd70851610848e718f7ada4b0a
Veemon <xxtheforgottenliesxx@gmail.com>
2016-11-02 14:55:48 +1000

    handled 404

--- app/app.js
+++ app/app.js
@@ -53,6 +53,7 @@ fs.readdirSync(routePath).forEach((file) => {
 
 // Catch 404 and forward to Error Handler
 app.use((req, res, next) => {
+  res.status(404).render('404', { url: req.originalUrl });
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
