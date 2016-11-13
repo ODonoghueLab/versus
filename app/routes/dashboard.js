2d398416a53ad395f4dbae1a9e641085c63d358c
Veemon <xxtheforgottenliesxx@gmail.com>
2016-11-13 19:42:48 +1000

    linted

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,18 +1,15 @@
-"use strict";
+
 
 const models = require('../models/index');
 
 module.exports = (app) => {
   // Primary User Dashboard
   app.get('/dashboard', (req, res) => {
-    
     // Find all experiments that belong to the current user.
     models.Experiment.findAll({
-      include: [{ model: models.User, where: { id: req.user.id }}]
-    }).then(experiments => {
-      res.render('dashboard', { username: req.user.name, experiments: experiments })
+      include: [{ model: models.User, where: { id: req.user.id } }],
+    }).then((experiments) => {
+      res.render('dashboard', { username: req.user.name, experiments });
     });
-    
   });
-  
 };
