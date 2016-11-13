f5db2be06586665108aa557171463f8fd38ef0c7
Tylor Stewart <trjstewart@gmail.com>
2016-11-13 11:43:18 +1000

    experiment dashboard and view

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,8 +1,19 @@
 "use strict";
 
+const models = require('../models/index');
+
 module.exports = (app) => {
   
   // Primary User Dashboard
-  app.get('/dashboard', (req, res) => res.render('dashboard'));
+  app.get('/dashboard', (req, res) => {
+    
+    // Find all experiments that belong to the current user.
+    models.Experiment.findAll({
+      include: [{ model: models.User, where: { id: req.user.id }}]
+    }).then(experiments => {
+      res.render('dashboard', { username: req.user.name, experiments: experiments })
+    });
+    
+  });
   
 };
