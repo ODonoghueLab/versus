90b6f2200658c25544a96645514ef64da24ca686
boscoh <apposite@gmail.com>
2017-06-23 15:39:45 +1000

    paths fix

--- server/src/app.js
+++ server/src/app.js
@@ -86,7 +86,7 @@ passport.use(new LocalStrategy((email, password, done) => {
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use('/api', require('./api'))
+app.use('/', require('./api'))
 
 // Catch 404 and forward to Error Handler
 app.use((req, res, next) => {
