ecfa4e6ff36447162fef91ebd3616d0fe183136b
boscoh <apposite@gmail.com>
2018-04-24 09:52:40 +1000

    home page tweaks

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -18,27 +18,28 @@
           padding-bottom: 2em;">
         <img
           style="
-            width: 100%"
+            width: 350px"
           src="../../static/logo.png">
       </div>
 
       <p>
-        Versus is a tool to facilitate the complete
-        ranking of a set of images for visual
-        quality analysis. It is designed to work both
-        locally and with
-        <a href="https://requester.mturk.com/">Mechanical Turk</a>.
+        Versus is a tool to facilitate visual analytics studies.
+        It is designed to work both as a standalone tool or to
+        use in conjunction with
+        <a href="https://requester.mturk.com/">Mechanical Turk</a>
+        as a survey link.
       </p>
 
       <p>
-        Currently, we
-        provide:
-        <ol>
-          <li> two-alternative forced-choice (2FCA)
-            option, whereby images a ranked via a series of comparisons
-            between two images.
+        Currently, we provide 2 options:
+      <ol>
+          <li>
+            Two-alternative forced-choice (2FCA) methodology to achieve a
+            full-set image ranking by comparing image pairs in several rounds.
           </li>
-          <li> multiple choice option with a qualification round
+          <li>
+            Multiple choice survey with a possible qualification
+            round to record participant answers.
           </li>
         </ol>
       </p>
@@ -50,14 +51,10 @@
       <p>
       </p>
 
-      <p>
-        To setup a set of images for analysis:
-      </p>
-
       <md-layout
-          md-align="left"
+          v-if="!user.authenticated"
+          md-align="le"
           md-vertical-align="center">
-        Please
         <router-link
             to="/login"
             tag="md-button"
@@ -79,7 +76,14 @@
 </template>
 
 <script>
+import auth from '../modules/auth'
+
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      user: auth.user,
+    }
+  }
 }
 </script>
